acfbfdf3563cf109de3a193d4d8e4df6e71e9712
Aitor Rodriguez Velilla <aitorro215@gmail.com>
2025-10-08 17:26:27 +0200

    Subida de sudoku relleno

--- JS/sudoku.js
+++ JS/sudoku.js
@@ -1,21 +1,75 @@
-'use strict';
+'use strict'
 
-var table = document.getElementById('SudokuBoard');
+var table = document.getElementById('SudokuBoard')
 
-if (table) {
-    for (var r = 0; r < 9; r++) {
-        for (var c = 0; c < 9; c++) {
-            var cell = document.createElement('div');
-            cell.classList.add('cell');
-
-            if (r % 3 === 0) cell.style.borderTop = '3px solid #000';
-            if (c % 3 === 0) cell.style.borderLeft = '3px solid #000';
-            if (r === 8) cell.style.borderBottom = '3px solid #000';
-            if (c === 8) cell.style.borderRight = '3px solid #000';
-
-            table.appendChild(cell);
+var board = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
+    //Como crear botones en ejecución
+    // for (let n = 1; n <= 9; n++){
+    //     const boton=document.createElement('div')
+    //     boton.classList.add('numero')
+    //     boton.textContent=n
+    // }
+for (var filas = 0; filas < 9; filas++) {
+    for (var columna = 0; columna < 9; columna++) {
+        var cell = document.createElement('div')
+        cell.classList.add('cell')
+        if (filas % 3 === 0) cell.style.borderTop = '3px solid #000'
+        if (columna % 3 === 0) cell.style.borderLeft = '3px solid #000'
+        if (filas === 8) cell.style.borderBottom = '3px solid #000'
+        if (columna === 8) cell.style.borderRight = '3px solid #000'
+        table.appendChild(cell)
+        if (board[filas][columna] !== '-') {
+            cell.textContent = board[filas][columna]
+            cell.classList.add('fixed');
+            cell.style.background="LightGray"
         }
+        // No esta 
+        //if(board[filas][columna]==solution){
+        //     alert("HAS GANADO OLE TU POLLA")
+        // }
     }
-} else {
-    console.error("No se encontró el div con id 'SudokuBoard'");
 }
+
+let selectedNumber = null
+
+const buttons = document.querySelectorAll('.CajaBotones button')
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(b => b.style.backgroundColor = '')
+        selectedNumber = button.textContent
+        button.style.backgroundColor = 'Orange'
+    })
+})
+
+const cells = document.querySelectorAll('#SudokuBoard .cell')
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if (selectedNumber && !cell.classList.contains('fixed')) {
+            cell.textContent = selectedNumber;
+            buttons.forEach(b => b.style.backgroundColor = '')
+            selectedNumber = null;
+        }
+    })
+})
+
